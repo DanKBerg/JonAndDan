7aef1cef1d9b4e130b3d91a62921b63b954c7fcf
Daniel Karlsrud Berg <daniel.karlsrud.berg@gmail.com>
2021-04-20 23:39:05 -0400

    Started adding some functionality for calendar

---
+++ app/src/main/java/fsu/csc3560/dkb/customcalendar/CalendarView.java
@@ -0,0 +1,65 @@
+package fsu.csc3560.dkb.customcalendar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+
+public class CalendarView extends LinearLayout{
+    LinearLayout header;
+    Button btnToday;
+    ImageView btnPrev;
+    ImageView btnNext;
+    TextView txtDateDay;
+    TextView txtDisplayDate;
+    TextView txtDateYear;
+    GridView gridView;
+
+    public CalendarView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        initControl(context, attrs);
+    }
+
+    private void assignUIElements() {
+        header = findViewById(R.id.calendar_header);
+        btnPrev = findViewById(R.id.calendar_prev_button);
+        btnNext = findViewById(R.id.calendar_next_button);
+        txtDateDay = findViewById(R.id.date_display_day);
+        txtDateYear = findViewById(R.id.date_display_year);
+        txtDisplayDate = findViewById(R.id.date_display_date);
+        btnToday = findViewById(R.id.date_display_today);
+        gridView = findViewById(R.id.calendar_grid);
+    }
+
+    private void initControl(Context context, AttributeSet attrs){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.activity_calender, this);
+        assignUIElements();
+    }
+
+    public void updateCalendar(HashSet<Date> events){
+        ArrayList<Date> cells = new ArrayList<>();
+        Calendar calendar = (Calendar)currentDate.clone();
+
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        int monthBeginningCell = calendar.get(Calendar.DAY_OF_WEEK) - 2;
+
+        calendar.add(Calendar.DAY_OF_MONTH, -monthBeginningCell);
+
+        while (cells.size() < DAYS_COUNT){
+            cells.add(calendar.getTime());
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        gridView.setAdapter(new CalendarAdapter(getContext(), cells, events));
+    }
+}
