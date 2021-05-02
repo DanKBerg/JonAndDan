8c109e1a87ed0c37520cbd06ed72ca5648376c8e
Daniel Karlsrud Berg <daniel.karlsrud.berg@gmail.com>
2021-05-02 00:40:41 -0400

    Workable buttons for day, month, and year

--- app/src/main/java/fsu/csc3560/dkb/customcalendar/CalenderActivity.java
+++ app/src/main/java/fsu/csc3560/dkb/customcalendar/CalenderActivity.java
@@ -14,6 +14,10 @@ public class CalenderActivity extends AppCompatActivity implements EditDialog.Ed
     private TextView month;
     private TextView day;
 
+    private Integer yearNumber;
+    private Integer monthNumber;
+    private Integer dayNumber;
+
     private String yearString;
     private String monthString;
     private String dayString;
@@ -56,8 +60,77 @@ public class CalenderActivity extends AppCompatActivity implements EditDialog.Ed
         monthString = String.valueOf(monthResult);
         dayString = String.valueOf(dayResult);
 
+        maximumNumMonth = monthMaximumResult;
+        maximumNumDay = dayMaximumResult;
+
         year.setText(yearString);
         month.setText(monthString);
+        day.setText(dayString);
+    }
+
+    public void PreviousYear(View view){
+        yearNumber = Integer.valueOf(year.getText().toString());
+        yearNumber -= 1;
+        yearString = String.valueOf(yearNumber);
+        year.setText(yearString);
+    }
+
+    public void NextYear(View view){
+        yearNumber = Integer.valueOf(year.getText().toString());
+        yearNumber += 1;
+        yearString = String.valueOf(yearNumber);
+        year.setText(yearString);
+    }
+
+    public void PreviousMonth(View view){
+        monthNumber = Integer.valueOf(month.getText().toString());
+        monthNumber -= 1;
+
+        if(monthNumber < 1){
+            PreviousYear(view);
+            monthNumber = maximumNumMonth;
+        }
+
+        monthString = String.valueOf(monthNumber);
+        month.setText(monthString);
+    }
+
+    public void NextMonth(View view){
+        monthNumber = Integer.valueOf(month.getText().toString());
+        monthNumber += 1;
+
+        if(monthNumber > maximumNumMonth){
+            NextYear(view);
+            monthNumber = 1;
+        }
+
+        monthString = String.valueOf(monthNumber);
+        month.setText(monthString);
+    }
+
+    public void PreviousDay(View view){
+        dayNumber = Integer.valueOf(day.getText().toString());
+        dayNumber -= 1;
+
+        if(dayNumber < 1){
+            PreviousMonth(view);
+            dayNumber = maximumNumDay;
+        }
+
+        dayString = String.valueOf(dayNumber);
+        day.setText(dayString);
+    }
+
+    public void NextDay(View view){
+        dayNumber = Integer.valueOf(day.getText().toString());
+        dayNumber += 1;
+
+        if(dayNumber > maximumNumDay){
+            NextMonth(view);
+            dayNumber = 1;
+        }
+
+        dayString = String.valueOf(dayNumber);
         day.setText(dayString);
     }
 }
